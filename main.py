3013ebe78b07f44d93c8fcc7e912a8615fe927b3
CuriousTony <aszdorovtsev@yandex.ru>
2024-10-28 16:10:50 +0300

    Finished the scraping and saving to csv logics.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,10 +11,17 @@ def get_data():
 
     try:
         raw_content = driver.find_elements(By.CSS_SELECTOR, 'div.WdR1o')
-        for content in raw_content:
-            name = content.find_element(By.CSS_SELECTOR, 'a.ui-GPFV8.qUioe.ProductName.ActiveProduct span').text
-            price = content.find_element(By.CSS_SELECTOR, 'span.ui-LD-ZU.KIkOH').text
-            link = content.find_element(By.CSS_SELECTOR, 'a.ui-GPFV8.qUioe.ProductName.ActiveProduct').get_attribute('href')
+        try:
+            with open('Kitchen_chairs.csv', 'w', encoding='utf-8-sig', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(['Название', 'Цена', 'Ссылка'])
+                for content in raw_content:
+                    name = content.find_element(By.CSS_SELECTOR, 'span[itemprop="name"]').text
+                    price = content.find_element(By.CSS_SELECTOR, 'span.ui-LD-ZU.KIkOH').text
+                    link = content.find_element(By.CSS_SELECTOR, 'a.ui-GPFV8.qUioe.ProductName.ActiveProduct').get_attribute('href')
+                    writer.writerow([name, price, link])
+        except Exception as ex:
+            print(f"Ошибка в ходе записи в файл - {ex}")
 
     except Exception as e:
         print(f"Ошибка в CSS селекторах - {e}")
